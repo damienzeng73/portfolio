e3b5b8cc722cad721bf9683ff66b53d5a2f5d3b1
damnee562 <damnee562@gmail.com>
2018-06-08 22:31:08 +0800

    Remove button in the menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import { Menu, Button } from 'semantic-ui-react'
+import { Container, Menu } from 'semantic-ui-react'
 import { Link } from 'react-scroll'
 
 import './Header.css'
 
 const Header = () => {
     return (
-        <div id='header'>
-            <Menu>
+        <Container id='header' fluid>
+            <Menu boderless stackable>
                 <Menu.Item header>Damien's portfolio</Menu.Item>
-                <Menu.Item position='right'>
-                    <a href='https://github.com/damnee562/portfolio' rel='noopener noreferrer'>
-                        <Button circular color='black' icon='github alternate' size='huge' />
-                    </a>
-                </Menu.Item>
             </Menu>
 
-            <Link spy smooth className='scroll' activeClass='active' to='test'>
+            <Link spy smooth className='scroll' activeClass='active' to='body'>
                 <span className='scroll-icon'></span>
                 <span className='scroll-icon'></span>
                 <span className='scroll-icon'></span>Scroll
             </Link>
-        </div>
+        </Container>
     )
 }
 
